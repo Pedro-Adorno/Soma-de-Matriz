ff931192a3eb7dcca641b224bc470935a9842047
Pedro Adorno <131396570+Pedro-Adorno@users.noreply.github.com>
2025-03-28 10:52:34 -0300

    Create soma_matrizes.py

---
+++ soma_matrizes.py
@@ -0,0 +1,41 @@
+linhas_a = int(input('Quantidade de linhas: '))
+colunas_a = int(input('Quantidade de colunas: '))
+
+matriz_a = []
+for i in range(linhas_a):
+    linha = []        
+    for j in range(colunas_a):
+        valor = int(input(f"Digite o valor para a posição [{i}][{j}]: "))
+        linha.append(valor)
+    matriz_a.append(linha)
+    
+for linha in matriz_a:
+    print(linha)
+linhas_b = int(input('Quantidade de linhas: '))
+colunas_b = int(input('Quantidade de colunas: '))
+
+matriz_b = []
+
+for i in range(linhas_b):
+    linha = []        
+    for j in range(colunas_b):
+        valor = int(input(f"Digite o valor para a posição [{i}][{j}]: "))
+        linha.append(valor)
+    matriz_b.append(linha)
+    
+for linha in matriz_b:
+    print(linha)
+if linhas_a != linhas_b or colunas_a != colunas_b:
+    print('As matrizes não possuem as mesmas dimensões e não podem ser somadas!')
+else:
+    matriz_c = []
+
+    for i in range(linhas_a):
+        linha = []
+        for j in range(colunas_a):
+            soma = matriz_a[i][j] + matriz_b[i][j]
+            linha.append(soma)
+        matriz_c.append(linha)
+
+    for linha in matriz_c:
+        print(linha)
